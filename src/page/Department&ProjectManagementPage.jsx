754f29b18c2b261bedf8b35a43302cb8626841c7
omartabbaa <omartabbaa0@gmail.com>
2024-11-10 23:14:08 +0100

    removing table from DepartmentProjectManagment

--- src/page/Department&ProjectManagementPage.jsx
+++ src/page/Department&ProjectManagementPage.jsx
@@ -1,12 +1,7 @@
 import GridMapComponent from "../Components/Lists/GridList/GridMapComponent";
 import "./Department&ProjectManagementPage.css";
 const DepartmentProjectManagementPage = () => {
-    const departments = [
-        "Department 1", "Department 2", "Department 3", "Department 4", "Department 5",
-        "Department 6", "Department 7", "Department 8", "Department 9", "Department 10",
-        "Department 11", "Department 12", "Department 13", "Department 14", "Department 15",
-        "Department 16", "Department 17", "Department 18", "Department 19", "Department 20"
-    ];
+
     return (
         <div>
 
